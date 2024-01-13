91cd71c70a47d81428359530d54337448d0a6c69
kellyjvan <kellyjvan@gmail.com>
2024-01-12 22:24:30 -0800

    Add membership page and update index.html.

--- components/header.js
+++ components/header.js
@@ -4,26 +4,38 @@ constructor() {
 };
   
     connectedCallback() {
-      this.innerHTML =   `    
-        <header class ="h">
-          <div class="row">          
-            <div class="dropdown d-block d-sm-none col">
-              <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">more</button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>         
-            </div>
-            <div class="d-block d-sm-none col-9 h-text"><h5 class="h-text">KSP</h5></div>
+      this.innerHTML =   `
+      <nav class="navbar navbar-expand-sm navbar-light bg-light">
+      <div class="container-fluid">
+          <a href="index.html" class="navbar-brand">Klamath Sportsman's Park</a>
+          <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div id="navbarCollapse" class="collapse navbar-collapse">
+              <ul class="nav navbar-nav">
+                  <li class="nav-item">
+                      <a href="membership.html" class="nav-link">Membership</a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="#" class="nav-link">Profile</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                      <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">More</a>
+                      <div class="dropdown-menu">
+                          <a href="#" class="dropdown-item">Inbox</a>
+                          <a href="#" class="dropdown-item">Drafts</a>
+                          <a href="#" class="dropdown-item">Sent Items</a>
+                          <div class="dropdown-divider"></div>
+                          <a href="#"class="dropdown-item">Trash</a>
+                      </div>
+                  </li>
+              </ul>
+              
           </div>
-        </header>        
+      </div>
+  </nav>
+  
 
-        <div class="d-none d-sm-block">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-        </div>   
     `;
 }
 }
